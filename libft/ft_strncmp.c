dac91bd17763c9f8014f617aa569839d22aa6c40
redgtxt <hp052635@gmail.com>
2024-04-09 20:40:17 +0100

    ft_strncmp corrigido

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -5,7 +5,7 @@ int ft_strncmp(const char *str1, const char *str2, size_t n)
     size_t i;
 
     i = 0;
-    while ((str1[i] != '\0' && str2[i] != '\0') && (i < n))
+    while ((str1[i] || str2[i] ) && (i < n))
     {
         if(str1[i] != str2[i])
             return ((unsigned char) str1[i] - (unsigned char) str2[i]);
