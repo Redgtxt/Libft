c539628d99579eb3bc7ebc7034c917dc5183f0e1
Hugo Miguel Guerreiro Pinto <hguerrei@c1r9s1.42lisboa.com>
2024-04-10 11:49:25 +0100

    itoa atualizado

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -7,8 +7,8 @@ static int ft_len(int n)
     count = 0;
     if(n <= 0)
         count++;
-    
-    while(n)
+
+    while(n != 0)
     {
         count++;
         n /= 10;
@@ -16,13 +16,17 @@ static int ft_len(int n)
     return (count);
 }
 
+// n = 123 / 10 = 12 | count = 1;
+// n = 12 / 10 = 1 | count = 2;
+// n = 1 / 10 = 0 | count = 3;
+// returnar (count);
 
-char *ft_itoa(int n)
+char *ft_itoa(int n) // n = -123
 {
     char    *ptr;
     int     len;
 
-    len = ft_len(n);
+    len = ft_len(n); // count = 4
     ptr = malloc(len + 1);
     if(ptr == NULL)
         return (NULL);
@@ -30,15 +34,22 @@ char *ft_itoa(int n)
     if(n == 0)
         ptr[0] = '0';
     else if (n < 0)
+    {
         ptr[0] = '-';
+        n *= -1;
+    }
     while(n)
     {
-        if(n < 0)
-            ptr[len--] = (~(n % 10) + 1) + 48;
-        else
-             ptr[len--] = (n % 10) + 48;
+        ptr[--len] = (n % 10) + 48;
         n /= 10;
     }
     return (ptr);
+}
+
+int main()
+{
+    char *teste = ft_itoa(-123445);
+    printf("%s\n", teste);
+    printf("%c\n", teste[2]);
 }
 
