7c2950143e8098134a8366b6e33c8b57b6916e66
Hugo Miguel Guerreiro Pinto <hguerrei@c1r9s1.42lisboa.com>
2024-04-10 12:25:45 +0100

    ft_join corrigido

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -2,40 +2,25 @@
 
 char *ft_strjoin(char const *s1, char const *s2)
 {
-    int i;
-    int j;
-    char *newStr;
-
-    if(!s1 || !s2)
+     if (!s1 || !s2)
         return NULL;
-
-    newStr = malloc((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char));
-    if(newStr == NULL)
+    size_t s1_len = ft_strlen(s1);
+    size_t s2_len = ft_strlen(s2);
+    char *newStr = malloc((s1_len + s2_len + 1) * sizeof(char));
+    if (newStr == NULL)
         return NULL;
-    i = 0;
-    while(s1[i] != '\0')
-    {
-        newStr[i] = s1[i];
-          i++;
-    }
-
-     j = 0;
-     while(s2[j] != '\0')
-    {
-        newStr[i + j] = s2[j];
-          j++;
-    }
-    newStr[i + j] = '\0';
+    ft_strlcpy(newStr, s1, s1_len + 1);
+    ft_strlcat(newStr, s2, s1_len + s2_len + 1);
     return newStr;
+
 }
-/*
-#include <stdio.h>
+
 int main(void)
 {
-    char s1[] = "Bom ";
+    char s1[] = "Bom    ";
     char s2[] = "Dia";
     char *juntos = ft_strjoin(s1,s2);
 
     printf("%s",juntos);
 }
-*/
+
