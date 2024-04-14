5e09641c97358547c3b695c89036db16bb64fd05
redgtxt <hp052635@gmail.com>
2024-04-14 21:34:26 +0100

    atoi corrigido

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -15,43 +15,31 @@
 
 #include "libft.h"
 
+
 int	ft_atoi(const char *str)
 {
-	int	convert_num;
-	convert_num = 1;
 	int	i;
+	int	neg;
+	int	num;
+
 	i = 0;
-	int	number;
-	number = 0;
-	int	count_sign;
-	count_sign = 0;
-
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+	neg = 1;
+	num = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	while (str[i] == '+' || str[i] == '-')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (count_sign > 0)
-			return (0);
-
 		if (str[i] == '-')
-			convert_num *= -1;
-
-		count_sign++;
+			neg = -1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		number = (number * 10) + str[i] - '0';
+		num = (str[i] - '0') + (num * 10);
 		i++;
 	}
-	return (number * convert_num);
+	return (num * neg);
 }
-
-/*
-#include <stdlib.h>
-
-
-
 
 
 int	main(void)
@@ -60,4 +48,4 @@ int	main(void)
 	printf(" True: %d\n",atoi(str));
 	printf(" Meu: %d\n",ft_atoi(str));
 }
-*/
+
